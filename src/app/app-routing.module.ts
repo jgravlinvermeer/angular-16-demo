6d639459bbe1408629c3b16bb373bbc53192a4c7
Jon Graf <jgraf@vermeer.com>
2024-01-08 18:14:18 -0600

    registered a route with the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RouterInputComponent} from "./components/router-input/router-input.component";
 
-const routes: Routes = [];
+const routes: Routes = [   {
+    path: "search",
+    component: RouterInputComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
