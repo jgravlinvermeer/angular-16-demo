9076a7d900cfa054a4ffd7bd8a714d826a06cfd7
Joseph R Gravlin <jgravlin@vermeer.com>
2024-01-10 10:33:02 -0500

    Use ActivatedRoute

--- src/app/components/router-input/router-input.component.ts
+++ src/app/components/router-input/router-input.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-router-input',
@@ -9,4 +10,13 @@ export class RouterInputComponent {
   @Input() data: string;
   @Input() routeParam: string | undefined;
   @Input() queryParam: string | undefined;
+
+  readonly routeParams$ = this.router.params;
+  readonly queryParams$ = this.router.queryParams;
+
+  constructor(private router: ActivatedRoute) { }
+
+  hasProps(x: any): boolean {
+    return Object.keys(x).length > 0;
+  }
 }
